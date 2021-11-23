acfc54ca24144f5518d476b8e6bc4c1aef335eb5
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-11-23 23:07:06 +0200

    updated navigation

--- react-apps/src/components/Header/Header.js
+++ react-apps/src/components/Header/Header.js
@@ -1,21 +1,22 @@
+import {Link, NavLink} from 'react-router-dom'
 const Header = () => {
     return (
         <header id="site-header">
             
             <nav className="navbar">
                 <section className="navbar-dashboard">
-                    <a href="#">Dashboard</a>
+                    <a href="/">Dashboard</a>
                     
                     <div id="guest">
-                        <a className="button" href="#">Login</a>
-                        <a className="button" href="#">Register</a>
+                        <Link to="/Login" className="button" >Login</Link>
+                        <Link to="/Register" className="button">Register</Link>
                     </div>
                     
                     <div id="user">
                         <span>Welcome, email</span>
-                        <a className="button" href="#">My Pets</a>
-                        <a className="button" href="#">Add Pet</a>
-                        <a className="button" href="#">Logout</a>
+                        <Link to="/my-pets" className="button">My Pets</Link>
+                        <Link to="/create" className="button">Add Pet</Link>
+                        <Link to="#" className="button">Logout</Link>
                     </div>
                 </section>
             </nav>
