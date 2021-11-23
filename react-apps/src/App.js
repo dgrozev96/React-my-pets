0551a432e2d410c4013472d57a08fceb0f68a75b
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-11-23 23:00:08 +0200

    added react-router

--- react-apps/src/App.js
+++ react-apps/src/App.js
@@ -1,20 +1,35 @@
+import { Routes, Route } from 'react-router-dom'
 import Header from "./components/Header";
 import Login from "./components/Login";
+import Register from "./components/Register";
+import MyPets from "./components/MyPets";
+import Dashboard from "./components/Dashboard";
+import CreatePage from "./components/CreatePage";
 
 function App() {
   return (
     <div id="container">
-        
-        <Header />
 
-        
-        <main id="site-content"></main>
+      <Header />
 
-        
-    
-        <footer id="site-footer">
-            <p>@PetMyPet</p>
-        </footer>
+
+      <main id="site-content">
+
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/my-pets" element={<MyPets />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+      
+      </main>
+
+      
+
+      <footer id="site-footer">
+        <p>@PetMyPet</p>
+      </footer>
 
     </div>
   );
